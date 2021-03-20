5cf213dd17e08740d6c7a85459043e1e26ab1b37
Marcin TORGiren Fabrykowski <torgiren@gmail.com>
2021-03-20 16:02:05 +0100

    make resolv conf changes permanemt - closes #125
    Signed-off-by: Marcin TORGiren Fabrykowski <torgiren@gmail.com>
    Change-Id: I257499bcb0adade2e61fb72c3214a12db46673ef
    Signed-off-by: Marcin TORGiren Fabrykowski <torgiren@gmail.com>

--- tests/test_centos7_shared/reboot.sh
+++ tests/test_centos7_shared/reboot.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-ansible -i libvirt-inventory.py test -b  -m reboot
-ansible -i libvirt-inventory.py test -m wait_for -a port=22
+ANSIBLE_CONFIG=fabsible.cfg ansible -i libvirt-inventory.py test -b  -m reboot
+ANSIBLE_CONFIG=fabsible.cfg ansible -i libvirt-inventory.py test -m wait_for -a port=22
